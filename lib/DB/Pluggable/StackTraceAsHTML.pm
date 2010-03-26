7aa31f67d54e2945efd26dfaf736ddaced260f3e
Marcel Grünauer <marcel@cpan.org>
2010-03-26 20:11:58 +0000

    Import of MARCEL/DB-Pluggable-StackTraceAsHTML-1.100850 from CPAN.
    gitpan-cpan-distribution: DB-Pluggable-StackTraceAsHTML
    gitpan-cpan-version:      1.100850
    gitpan-cpan-path:         MARCEL/DB-Pluggable-StackTraceAsHTML-1.100850.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/DB/Pluggable/StackTraceAsHTML.pm
+++ lib/DB/Pluggable/StackTraceAsHTML.pm
@@ -1,13 +1,15 @@
-package DB::Pluggable::StackTraceAsHTML;
-use 5.006;
+use 5.008;
 use strict;
 use warnings;
+
+package DB::Pluggable::StackTraceAsHTML;
+our $VERSION = '1.100850';
+# ABSTRACT: Add debugger command to see stack trace as HTML
 use Devel::StackTrace::WithLexicals;
 use Devel::StackTrace::AsHTML;
 use File::Slurp qw(write_file);
 use Browser::Open qw(open_browser);
-our $VERSION = '0.01';
-use base qw(DB::Pluggable::Plugin);
+use parent qw(DB::Pluggable::Plugin);
 
 sub register {
     my ($self, $context) = @_;
@@ -22,15 +24,21 @@ sub register {
     );
 }
 1;
-__END__
 
-=for test_synopsis
-1;
+
+__END__
+=pod
+
+=for test_synopsis 1;
 __END__
 
 =head1 NAME
 
 DB::Pluggable::StackTraceAsHTML - Add debugger command to see stack trace as HTML
+
+=head1 VERSION
+
+version 1.100850
 
 =head1 SYNOPSIS
 
@@ -61,7 +69,6 @@ DB::Pluggable::StackTraceAsHTML - Add debugger command to see stack trace as HTM
     ...
       DB<1> Th
 
-
 =head1 DESCRIPTION
 
 This plugin for L<DB::Pluggable> adds the C<Th> command to the debugger, which
@@ -74,46 +81,43 @@ stack trace and C<h> indicates that the output is HTML.
 
 =head1 METHODS
 
-=over 4
-
-=item C<register>
+=head2 register
 
 Adds the C<Th> command to the Perl debugger.
 
-=back
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
 Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=DB-Pluggable-StackTraceAsHTML>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN site
-near you. Or see
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
 L<http://search.cpan.org/dist/DB-Pluggable-StackTraceAsHTML/>.
 
 The development version lives at
-L<http://github.com/hanekomu/db-pluggable-stacktraceashtml/>.  Instead of
-sending patches, please fork this project using the standard git and github
-infrastructure.
+L<http://github.com/hanekomu/DB-Pluggable-StackTraceAsHTML/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
-=head1 AUTHORS
+=head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2009 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2009 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
+
